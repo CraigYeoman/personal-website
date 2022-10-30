43c16375be2e6fa1e417702309aaaf14a366c007
CraigYeoman <yeomancraig@gmail.com>
2022-10-30 14:45:34 -0500

    github link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,22 @@
+import { AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from "react-icons/ai";
+import { IoMdDocument } from "react-icons/io";
+
 const Header = () => {
     return (
         <div className="header">
+            <ul>
+                <li>
+                    <a className="header--link" href="https://github.com/CraigYeoman" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
+                </li>
+                <li>
+                    <a className="header--link" href="https://www.linkedin.com/in/craig-yeoman-10341a8b/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
+                </li>
+                <li>
+                <a className="header--link" href="https://www.linkedin.com/in/craig-yeoman-10341a8b/" target="_blank" rel="noopener noreferrer"><IoMdDocument /></a>
+                </li>
+            </ul>
+            
             <ul>
                 <li>
                     <a className="header--link" href="#main" to='/'>Home</a>
